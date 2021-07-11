d9bc7b0ee54ebb1794b7790a54a51bf71f7bdb10
CodingLukas <lukasimbm@gmail.com>
2021-07-11 12:12:58 +0300

    lesson 24 Rendering Activity With Polymorphism

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -36,7 +36,7 @@ class Project extends Model
 
     public function activity(): HasMany
     {
-        return $this->hasMany(Activity::class);
+        return $this->hasMany(Activity::class)->latest();
     }
 
     public function recordActivity($description)
